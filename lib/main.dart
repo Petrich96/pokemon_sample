00174f260cdae3604f944d72e50c6f7af5002c27
work <me@example.com>
2022-03-13 23:27:03 +0300

    Добавлено кеширование json с информацией

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart'  as pathprovider;
 import 'package:flutter/material.dart';
 import 'package:pokemon/MainScreen.dart';
 import 'pokemon.dart';
 import 'pokemon_repository.dart';
 import 'PokemonDetailScreen.dart';
 import 'package:provider/provider.dart';
+import 'package:hive/hive.dart';
 
-void main() {
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+var path = await pathprovider.getApplicationDocumentsDirectory();
+print(path.path);
+  Hive.init(path.path);
   runApp(const MyApp());
 }
 
--- lib/pokemon_repository.dart
+++ lib/pokemon_repository.dart
@@ -8,27 +8,35 @@ import 'package:hive/hive.dart';
 class PokemonRepo extends ChangeNotifier {
   Map<String, Pokemon> pokemons = {};
   Map<String, String> pokemonsName = {};
+
   //вот локальная база данных вместо Map<String, Pokemon> pokemons = {};
-  var poke = Hive.box("pokemons");
+  // var poke = Hive.openBox("pokemons");
 
   Future<Pokemon> getPokemonById(String id) async {
     // if (pokemons.containsKey(id))
     // return Future.delayed(Duration.zero, () => pokemons[id]!);
     //теперь проверяем наличия JSON в локальной базе
+    var poke = await Hive.openBox<String>("pokemons");
     if (poke.containsKey(id))
       return _getPokemonFromStorage(id);
     else
       return this._downloadPokemonInfoById(id);
   }
+
 //появилась новая функция которая достает значение из локальной базы
   Future<Pokemon> _getPokemonFromStorage(String id) {
-    return Future.delayed(Duration.zero, () => Pokemon.fromJson(jsonDecode(poke.get(id))));
+    return Future.delayed(Duration.zero, () async {
+      var poke = await Hive.openBox<String>("pokemons");
+      return Pokemon.fromJson(jsonDecode(poke.get(id) as String));
+    });
   }
 
   Future<Pokemon> _downloadPokemonInfoById(String id) async {
     var url = Uri.https('pokeapi.co', '/api/v2/pokemon/$id', {'q': '{https}'});
     Pokemon pokemon = Pokemon();
     var response = await http.get(url);
+    var poke = await Hive.openBox<String>("pokemons");
+
     if (response.statusCode == 200) {
       //скачанный текст кладем в локальной базе
       poke.put(id, response.body);
